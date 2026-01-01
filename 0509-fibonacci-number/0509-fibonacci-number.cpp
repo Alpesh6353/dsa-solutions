850b4dc44acee734f6ebe1d66f96d760dad3e83f
Alpesh6353 <alphesrathod098@gmail.com>
2026-01-01 16:50:37 +0530

    Time: 9 ms (46.07%), Space: 7.8 MB (63.96%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fibo(int n){
+        if(n==1 || n==2 ) return 1;
+        
+        return fibo(n-1) + fibo(n-2);
+    }
+    int fib(int n) {
+        if(n==0) return 0;
+        return fibo(n);
+    }
+};
