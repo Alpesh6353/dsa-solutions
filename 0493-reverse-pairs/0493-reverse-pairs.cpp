fd78526447d7c4c4605ec4942cd4bd0443c3643a
Alpesh6353 <alphesrathod098@gmail.com>
2026-02-11 17:25:55 +0530

    Time: 341 ms (72.53%), Space: 174.8 MB (69.09%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,52 @@
+
+class Solution {
+public:
+int count =0;
+int inversion(vector<long long> &a,vector<long long>& b){
+    int c=0;
+    int i=0;
+    int j=0;
+    while(i<a.size() && j<b.size()){
+        if(a[i]>2*b[j]){
+            c += (a.size()-i);
+            j++;
+        }
+        else i++;
+    }
+    return c;
+}
+void sort(vector<long long>& a,vector<long long>& b,vector<long long>& v){
+    int m = a.size();
+    int n = b.size();
+    int i=0,j=0,k=0;
+    while(i<m && j<n){
+        if(a[i]>=b[j]) v[k++] = b[j++];
+        else v[k++] = a[i++];
+    }
+    if(i==m) while(j<n) v[k++] = b[j++];
+    if(j==n) while(i<m) v[k++] = a[i++];
+}
+void mergesort(vector<long long>& v){
+    int n = v.size();
+    if(n==1) return;
+    int n1 = n/2,n2=n-n/2;
+    vector<long long> a(n1),b(n2);
+    for(int i=0;i<n1;i++){
+        a[i] = v[i];
+    }
+    for(int i=0;i<n2;i++){
+        b[i] = v[i+n1];
+    }
+    mergesort(a);
+    mergesort(b);
+    count += inversion(a,b);
+    sort(a,b,v);
+    a.clear();
+    b.clear();
+}
+    int reversePairs(vector<int>& nums) {
+        vector<long long> v(nums.begin(),nums.end());
+        mergesort(v);
+        return count;
+    }
+};
