67a22deed00f8b5d6c3e183e51829c23688af68e
Alpesh6353 <alphesrathod098@gmail.com>
2026-03-03 20:02:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void fillsieve(vector<bool>& sieve){
         int n = sieve.size()-1;
-        for(int i=2;i<=sqrt(n);i++){
+        for(int i=2;i*i<=n;i++){
             for(int j = i*2;j<=n;j+=i){
                 sieve[j]=0;
             }
@@ -12,11 +12,10 @@ public:
         if(n<=2) return 0;
         n = n-1;
         int count = 0;
-        vector<int> v(n+1);
         vector<bool> sieve(n+1,1);  // 1 means prime,0 will meant not prime;
         fillsieve(sieve);
         sieve[0]=0;
-        sieve[1]=1;
+        sieve[1]=0;
         for(int i=2;i<=n;i++){
             if(sieve[i]==1) count++;
         }
