1039a2b6bf9c053bf70fc200f4ec01e31ee8ac27
Alpesh6353 <alphesrathod098@gmail.com>
2026-03-11 13:07:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.length();
         string ans = "";
         for(int i=0;i<n;i++){
-            if((s[i]>='0' && s[i]<='9') || (s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<123)) {
+            if((s[i]>='0' && s[i]<='9') || (s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z')) {
                 ans += tolower(s[i]);
             }
         }
