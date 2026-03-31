9a18a0fed46e60ca20e857db19c3c51c90de90a4
Alpesh6353 <alphesrathod098@gmail.com>
2026-03-31 17:46:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,8 +5,9 @@ public:
         int left = 0;
         int right = n-1;
         int maxw = INT_MIN;
+        int Area = 0;
         while(right>left){
-           int Area = min(height[left],height[right]) * (right - left);
+           Area = min(height[left],height[right]) * (right - left);
            maxw = max(maxw,Area);
            if(height[left]>=height[right]) right--;
            else left++;
