533b35feef73e66ba83c836a5b2fb54eefc9cdbe
Alpesh6353 <alphesrathod098@gmail.com>
2026-04-09 17:52:22 +0530

    Time: 0 ms (100%), Space: 19.6 MB (26.68%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -27,17 +27,13 @@ public:
             temp = temp->next;
         }
         if(list1 == NULL){
-            while(list2 != NULL){
+            if(list2 != NULL){
                 temp->next = list2;
-                list2 = list2->next;
-                temp = temp->next;
             }
         }
         if(list2 == NULL){
-            while(list1 != NULL){
+            if(list1 != NULL){
                 temp->next = list1;
-                list1 = list1->next;
-                temp = temp->next;
             }
         }
         return dummy->next;
