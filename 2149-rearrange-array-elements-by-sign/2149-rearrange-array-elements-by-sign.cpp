166221eed1ae07abbecc0657c7a944315d3c080d
Alpesh6353 <alphesrathod098@gmail.com>
2026-05-28 21:51:01 +0530

    Time: 4 ms (78.24%), Space: 128.3 MB (77.77%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -6,16 +6,15 @@ public:
         int i=0;
         int j=1;
         int k = 0;
-        while(n>i){
-            if(nums[i]>0){
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){ // for even
                 v[k] = nums[i];
                 k += 2;
             }
-            else {
+            else {  // for odd
                 v[j] = nums[i];
                 j += 2;
             }
-            i++;
         }
         return v;
     }
