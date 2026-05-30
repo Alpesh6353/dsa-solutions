64743cf16955b08ef92bb543da7d77f052231f9e
Alpesh6353 <alphesrathod098@gmail.com>
2026-05-30 20:04:31 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.35%) - LeetHub

--- 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.cpp
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.cpp
@@ -5,8 +5,7 @@ public:
         for(int i=0;i<n-1;i++){
             int x = s[i] - '0';
             int y = s[i+1] - '0';
-            if(abs(x-y)<=2) continue;
-            else return false;
+            if(abs(x-y)>2) return false;
         }
         return true;
     }
