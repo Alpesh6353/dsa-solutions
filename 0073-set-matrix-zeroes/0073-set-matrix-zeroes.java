9d2b38fa75f1e376c1d01c96bf415988b524e66b
Alpesh6353 <alphesrathod098@gmail.com>
2026-06-26 11:54:26 +0530

    Time: 1 ms (98.49%), Space: 47.4 MB (90.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,23 +2,30 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] res = new int[m][n];
+        int[] ar = new int[m];
+        int[] ac = new int[n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    for(int k=0;k<n;k++){
-                        res[i][k] = 1;
-                    }
-                    for(int k=0;k<m;k++){
-                        res[k][j] = 1;
-                    }
+                    ar[i] = 1;
+                    ac[j] = 1;
                 }
             }
         }
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(res[i][j]==1) matrix[i][j] = 0;
+            if(ar[i]==1){
+                for(int k=0;k<n;k++){
+                    matrix[i][k]=0;
+                }
             }
         }
+        for(int i=0;i<n;i++){
+            if(ac[i]==1){
+                for(int k=0;k<m;k++){
+                    matrix[k][i]=0;
+                }
+            }
+        }
+
     }
 }
