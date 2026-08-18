651bd01743781cc9944c5f0f2899fc2c99d379f7
Alpesh6353 <alphesrathod098@gmail.com>
2026-08-18 21:04:59 +0530

    Time: 14 ms (39.02%), Space: 48.1 MB (25.37%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -4,24 +4,24 @@ class Solution {
         int max = Integer.MIN_VALUE;
         int sum = 0;
         for(int i=0;i<n;i++){
-            if(piles[i]>max) max = piles[i];
+            if(max<piles[i]) max = piles[i];
             sum += piles[i];
         }
         int lo = 1;
         int hi = max;
-        int ans = 0;
+        int ans = 1;
         while(lo<=hi){
             int mid = lo + (hi-lo)/2;
             long hours = 0;
-            int sum2 = 0;
+            int currentsum = 0;
             for(int i=0;i<n;i++){
                 if(piles[i]%mid==0){
-                    sum2 = piles[i]/mid;
-                    hours += sum2;
+                    currentsum = piles[i]/mid;
+                    hours += currentsum;
                 }
                 else{
-                    sum2 = piles[i]/mid + 1;
-                    hours += sum2;
+                    currentsum = piles[i]/mid + 1;
+                    hours += currentsum;
                 }
             }
             if(hours<=h){
